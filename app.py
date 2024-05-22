6580389551c15b4b6e1efa8f1764f5130e310448
isakshay007 <117896902+isakshay007@users.noreply.github.com>
2024-05-22 02:13:27 -0700

    Adding Files

---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+from lyzr_automata.ai_models.openai import OpenAIModel
+from lyzr_automata import Agent, Task
+from lyzr_automata.pipelines.linear_sync_pipeline import LinearSyncPipeline
+from PIL import Image
+from lyzr_automata.tasks.task_literals import InputType, OutputType
+import os
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = st.secrets["apikey"]
+
+st.markdown(
+    """
+    <style>
+    .app-header { visibility: hidden; }
+    .css-18e3th9 { padding-top: 0; padding-bottom: 0; }
+    .css-1d391kg { padding-top: 1rem; padding-right: 1rem; padding-bottom: 1rem; padding-left: 1rem; }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+image = Image.open("./logo/lyzr-logo.png")
+st.image(image, width=150)
+
+# App title and introduction
+st.title("TroubleShoot Assistant🤖")
+st.markdown("Welcome to the TroubleShoot Assistant! Just describe your issue, and receive clear, step-by-step guidance to fix it.")
+input = st.text_input("Please enter the issue or problem you are facing:",placeholder=f"""Type here""")
+
+open_ai_text_completion_model = OpenAIModel(
+    api_key=st.secrets["apikey"],
+    parameters={
+        "model": "gpt-4-turbo-preview",
+        "temperature": 0.2,
+        "max_tokens": 1500,
+    },
+)
+
+
+def generation(input):
+    generator_agent = Agent(
+        role="Expert TROUBLESHOOTING ASSISTANT",
+        prompt_persona=f"Your task is to ADDRESS USER QUERIES related to technical issues they are encountering and PROVIDE SOLUTIONS or steps to RESOLVE those issues.")
+    prompt = f"""
+You are an Expert TROUBLESHOOTING ASSISTANT. Your task is to ADDRESS USER QUERIES related to technical issues they are encountering and PROVIDE SOLUTIONS or steps to RESOLVE those issues.
+
+Here's how you should approach each query:
+
+1. GATHER all necessary information from the user about the issue their facing.
+
+2. ANALYZE the information provided to IDENTIFY the root cause of the problem.
+
+3. DEVELOP a step-by-step STRATEGY for resolving the issue, ensuring that each step is CLEAR and EASY TO FOLLOW.
+
+4. COMMUNICATE these steps to the user in a SUPPORTIVE and UNDERSTANDING manner, ensuring they feel heard and assisted.
+
+You MUST maintain PATIENCE and PROFESSIONALISM throughout each interaction, aiming to deliver EFFECTIVE resolutions that enhance user satisfaction.
+
+ """
+
+    generator_agent_task = Task(
+        name="Generation",
+        model=open_ai_text_completion_model,
+        agent=generator_agent,
+        instructions=prompt,
+        default_input=input,
+        output_type=OutputType.TEXT,
+        input_type=InputType.TEXT,
+    ).execute()
+
+    return generator_agent_task 
+   
+if st.button("TroubleShoot!"):
+    solution = generation(input)
+    st.markdown(solution)
+
+with st.expander("ℹ️ - About this App"):
+    st.markdown("""
+    This app uses Lyzr Automata Agent . For any inquiries or issues, please contact Lyzr.
+
+    """)
+    st.link_button("Lyzr", url='https://www.lyzr.ai/', use_container_width=True)
+    st.link_button("Book a Demo", url='https://www.lyzr.ai/book-demo/', use_container_width=True)
+    st.link_button("Discord", url='https://discord.gg/nm7zSyEFA2', use_container_width=True)
+    st.link_button("Slack",
+                   url='https://join.slack.com/t/genaiforenterprise/shared_invite/zt-2a7fr38f7-_QDOY1W1WSlSiYNAEncLGw',
+                   use_container_width=True)
